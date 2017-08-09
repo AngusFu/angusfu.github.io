95c9c1feb555a881a197eb3b113d82636af0c635
文蔺 <AngusFu@users.noreply.github.com>
2017-08-09 18:02:08 +0800

    Update invertWorker.js

--- examples/arraybuffer/invertWorker.js
+++ examples/arraybuffer/invertWorker.js
@@ -1,1 +1,9 @@
-invertWorker.js
+onmessage = function (e) {
+  const data = e.data;
+  for (var i = 0; i < data.length; i += 4) {
+    data[i]     = 225 - data[i];     // red
+    data[i + 1] = 225 - data[i + 1]; // green
+    data[i + 2] = 225 - data[i + 2]; // blue
+  }  
+  postMessage(data);
+}
